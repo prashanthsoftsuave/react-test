0661f12baf557b4328e86f62ec18a9d1d4a9faa1
prashanthrajendiran <prashanth.rajendiran@impigertech.com>
2022-10-03 18:08:55 +0530

    homepage change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          test firebase project github auto deploy firebase netlify
+          welcome
         </p>
         <a
           className="App-link"
